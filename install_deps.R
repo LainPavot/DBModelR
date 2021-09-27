8dc56d99f357ad674f642a457edff55f18becef9
lain <lain.pavot@protonmail.com>
2021-09-27 18:35:40 +0200

    feat(script:install): add desps install script

---
+++ install_deps.R
@@ -0,0 +1,37 @@
+
+
+if (!requireNamespace("devtools", quietly=TRUE)) {
+    install.packages("devtools", repos="https://cran.r-project.org/")
+}
+
+if (!requireNamespace("DBI", quietly=TRUE)) {
+    install.packages("DBI", repos="https://cran.r-project.org/")
+}
+
+if (!requireNamespace("RSQLite", quietly=TRUE)) {
+    install.packages("RSQLite", repos="https://cran.r-project.org/")
+}
+
+if (!requireNamespace("testthat", quietly=TRUE)) {
+    install.packages("testthat", repos="https://cran.r-project.org/")
+}
+
+if (!requireNamespace("knitr", quietly=TRUE)) {
+    install.packages("knitr", repos="https://cran.r-project.org/")
+}
+
+if (!requireNamespace("blob", quietly=TRUE)) {
+    install.packages("blob", repos="https://cran.r-project.org/")
+}
+
+if (!requireNamespace("Cairo", quietly=TRUE)) {
+    install.packages("Cairo", repos="https://cran.r-project.org/")
+}
+
+if (!requireNamespace("qpdf", quietly=TRUE)) {
+    install.packages("qpdf", repos="https://cran.r-project.org/")
+}
+
+if (!requireNamespace("rmarkdown", quietly=TRUE)) {
+    install.packages("rmarkdown", repos="https://cran.r-project.org/")
+}
